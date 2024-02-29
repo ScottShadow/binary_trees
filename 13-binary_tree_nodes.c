ef587944600909e3d1066360100f6d54d79ef0cb
unknown <rudaseswascott@gmail.com>
2024-02-29 18:23:07 +0200

    added 13 14

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,14 +1,32 @@
 #include "binary_trees.h"
 /**
- * binary_tree_nodes - Counts the number of nodes in a binary tree.
- * @tree: A pointer to the root node of the tree to count the nodes.
+ * binary_tree_nodes - Counts the number of nodes with at least 1 child
+ *                     in a binary tree.
+ * @tree: A pointer to the root node of the binary tree.
  *
- * Return: The number of nodes in the tree, or 0 if @tree is NULL.
+ * Description: This function counts the number of nodes in a binary tree
+ * that have at least one child, excluding leaf nodes. If the tree is NULL,
+ * the function returns 0, and a NULL pointer is not considered a node.
+ *
+ * Return: The number of nodes with at least one child in the binary tree,
+ *         or 0 if the tree is NULL.
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	size_t nodes = 0;
+
+	/* Check if the tree is NULL */
 	if (tree == NULL)
 		return (0);
 
-	return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	/* If the current node has atleast one chid, increment the nodes count */
+	if (tree->left != NULL || tree->right != NULL)
+		nodes++;
+
+	/* Recursively count the nodes in the left and right subtrees */
+	nodes += binary_tree_nodes(tree->left);
+	nodes += binary_tree_nodes(tree->right);
+
+	/* Return the total number of nodes */
+	return (nodes);
 }
--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,10 +1,35 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: A pointer to the root node of the binary tree.
+ *
+ * Description: This function calculates the height of a binary tree,
+ * which is the length of the longest path from the root node to any leaf.
+ * If the tree is NULL, the function returns 0.
+ *
+ * Return: The height of the binary tree, or 0 if the tree is NULL.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	/* Check if the tree is NULL */
+	if (tree == NULL)
+		return (0);
+	/* Calculate the height of the left subtree */
+	left_height = binary_tree_height(tree->left);
+	/* Calculate the height of the right subtree */
+	right_height = binary_tree_height(tree->right);
+	/* Return the maximum height of the left and right subtrees, plus 1*/
+	return (1 + (left_height > right_height ? left_height : right_height));
+}
+
+/**
  * binary_tree_balance - Calculates the balance factor of a binary tree node.
  * @tree: A pointer to the root node of the binary tree.
  *
- * Return: The balance factor of the binary tree node, or 0 if the tree is NUL
+ * Return: balance factor of the binary tree node, or 0 if the tree is NULL.
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
