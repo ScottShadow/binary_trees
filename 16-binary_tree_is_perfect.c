64f14bf567292fa8f92a434cd9620ca51abe7dbc
unknown <rudaseswascott@gmail.com>
2024-02-29 11:37:06 +0200

    added functions

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,37 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_height - Computes the height of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the height.
+ *
+ * Return: The height of the tree, or 0 if @tree is NULL.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t left_height, right_height;
+
+    if (tree == NULL)
+        return (0);
+
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
+
+    return (1 + (left_height > right_height ? left_height : right_height));
+}
+
+/**
+ * binary_tree_nodes - Counts the number of nodes in a binary tree.
+ * @tree: A pointer to the root node of the tree to count the nodes.
+ *
+ * Return: The number of nodes in the tree, or 0 if @tree is NULL.
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return (0);
+
+    return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+}
+/**
  * is_perfect_recursive - Checks if a binary tree is perfect recursively.
  * @tree: A pointer to the root node of the tree to check.
  * @depth: The depth of the current node.
