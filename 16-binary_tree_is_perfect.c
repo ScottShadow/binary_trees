f83c712f1cfc063db16bd06dbd3fd39c69e87bea
unknown <rudaseswascott@gmail.com>
2024-02-29 10:42:38 +0200

    special case for single node handled

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,7 +7,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
         return (0);
 
     if ((!tree->left && !tree->right) && tree->parent == NULL)
-        return (1);
+        return (0);
 
     is_left_perfect = binary_tree_is_perfect(tree->left);
     is_right_perfect = binary_tree_is_perfect(tree->right);
